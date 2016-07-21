263e7d1672d948dd7184598333831c023ba471a4
Michael Falanga <mfalanga@manifestcorp.com>
2016-07-21 16:54:25 -0400

    Install DI

--- STAR.Web/Controllers/ContractorController.cs
+++ STAR.Web/Controllers/ContractorController.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using STAR.Data;
+using STAR.Domain;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace STAR.Web.Controllers {
     public class ContractorController : Controller {
+        private StarContext context;
+
+        public ContractorController(DbContext context) {
+            this.context = context as StarContext;
+        }
+
         public ActionResult Index() {
-            return View();
+            return View(context.Contractors.ToList());
         }
 
         public ActionResult Details() {
