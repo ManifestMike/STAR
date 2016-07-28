ba5dd0d42ed548d51ade3750ba2626ae337c88a8
James Zhang <jlz24@case.edu>
2016-07-28 13:40:32 -0400

    Able to search for contractors by skill on Details page. Need to change it to do what we want

--- STAR.Web/Controllers/ContractorController.cs
+++ STAR.Web/Controllers/ContractorController.cs
@@ -21,9 +21,37 @@ namespace STAR.Web.Controllers {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Details(string searchTerm)
+        {
+            //query
+            ViewBag.SearchTerm = searchTerm;
+
+            using (context)
+            {
+                context.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+                var contractors = context.Contractors
+                              .Include(c => c.Skills)
+                              .Where(c => c.Skills.Any(s => s.Name == searchTerm)).ToList();
+
+                return View(contractors);
+            }
+        }
+
+
         public ActionResult Search() {
 
             return View(context.Contractors.ToList());
+        }
+
+        public JsonResult GetSkills(string term)
+        {
+            List<string> skills;
+
+            skills = context.Skills.Where(x => x.Name.StartsWith(term))
+                .Select(y => y.Name).ToList();
+
+            return Json(skills, JsonRequestBehavior.AllowGet);
         }
     }
 }
