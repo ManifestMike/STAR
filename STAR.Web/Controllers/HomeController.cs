4b4173e94dfef745cc615c9e798bcf01d9031659
Michael Falanga <mfalanga@manifestcorp.com>
2016-07-29 10:12:51 -0400

    Move contractor search from HomeController to ContractorController

--- STAR.Web/Controllers/HomeController.cs
+++ STAR.Web/Controllers/HomeController.cs
@@ -1,35 +1,9 @@
-﻿using STAR.Data;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web.Mvc;
-using System.Data.Entity;
-using STAR.Data;
+﻿using System.Web.Mvc;
 
 namespace STAR.Web.Controllers {
     public class HomeController : Controller {
-        private StarContext context;
-
-        public HomeController(DbContext context) {
-            this.context = context as StarContext;
-        }
-
         public ActionResult Index() {
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult Index(string searchTerm) {
-            ViewBag.SearchTerm = searchTerm;
-            //given a skill return all contractors with that skill
-            using (context) {
-                context.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
-                var contractors = context.Contractors
-                              .Include(c => c.Skills)
-                              .Where(c => c.Skills.Any(s => s.Name == searchTerm)).ToList();
-
-                return View(contractors);
-            }
         }
     }
 }
