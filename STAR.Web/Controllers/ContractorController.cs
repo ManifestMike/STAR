ef5d6abba5f285e1ab8ea6bd2d6e9cc00fafb4f3
Michael Falanga <mfalanga@manifestcorp.com>
2016-08-01 12:35:52 -0400

    Formatting

--- STAR.Web/Controllers/ContractorController.cs
+++ STAR.Web/Controllers/ContractorController.cs
@@ -1,24 +1,17 @@
-﻿using Extensions;
-using STAR.Data;
+﻿using STAR.Data;
 using STAR.Domain;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
-namespace STAR.Web.Controllers
-{
-    public class ContractorController : Controller
-    {
+namespace STAR.Web.Controllers {
+    public class ContractorController : Controller {
         private StarContext context;
 
-        public ContractorController(DbContext context)
-        {
+        public ContractorController(DbContext context) {
             this.context = context as StarContext;
         }
 
-        
         public ActionResult Index(int[] selectedSkillIds) {
             if (selectedSkillIds != null) {
                 var convertedIds = selectedSkillIds.ToList();
@@ -26,7 +19,6 @@ namespace STAR.Web.Controllers
 
                 //given a list of skills return all contractors with those skills
                 using (context) {
-                    context.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
                     //Query 
                     var contractors = context.Contractors
                                   .Include(c => c.Skills)
@@ -43,72 +35,55 @@ namespace STAR.Web.Controllers
         }
 
         //autopopulate Details
-        public ActionResult Details(int? id)
-        {
-            if (!id.HasValue)
-            {
+        public ActionResult Details(int? id) {
+            if (!id.HasValue) {
                 return View();
             }
-            context.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+
             var contractor = context.Contractors.Include(c => c.Skills).Where(c => c.ID == id).FirstOrDefault();
-            
             return View(contractor);
         }
 
         //Update/Save Details
         [HttpPost]
-        public ActionResult Details(Contractor contractor)
-        {
-            if (contractor.ID==0)
-            {
-                context.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+        public ActionResult Details(Contractor contractor) {
+            if (contractor.ID == 0) {
                 var contractors = context.Contractors.Include(c => c.Skills).Where(c => c.FirstName == contractor.FirstName).Where(c => c.LastName == contractor.LastName).FirstOrDefault();
 
-                if (context.Contractors.Any(x => x.FirstName == contractor.FirstName && x.LastName == contractor.LastName))
-                {
+                if (context.Contractors.Any(x => x.FirstName == contractor.FirstName && x.LastName == contractor.LastName)) {
                     return View();
                 }
-                context.Contractors.Add(new Domain.Contractor
-                { FirstName = contractor.FirstName, LastName = contractor.LastName });
+                context.Contractors.Add(new Domain.Contractor { FirstName = contractor.FirstName, LastName = contractor.LastName });
 
                 context.SaveChanges();
 
                 return View();
             }
-            else
-            {
+            else {
                 Contractor updatedContractor = context.Contractors.Where(c => c.ID == contractor.ID).FirstOrDefault();
                 updatedContractor.FirstName = contractor.FirstName;
                 updatedContractor.LastName = contractor.LastName;
-                //updatedContractor.Skills =
-
 
                 context.SaveChanges();
 
                 return View(updatedContractor);
             }
-
         }
 
-
         [HttpGet]
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Delete(int? id) {
+            if (id == null) {
                 return View();
             }
             Contractor contractor = context.Contractors.Find(id);
-            if (contractor == null)
-            {
+            if (contractor == null) {
                 return HttpNotFound();
             }
             return View(contractor);
         }
 
         [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {
+        public ActionResult DeleteConfirmed(int id) {
             Contractor contractor = context.Contractors.Find(id);
             context.Contractors.Remove(contractor);
             context.SaveChanges();
