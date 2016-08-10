9d959013677ca4d0503b8cd069667f30347ee92d
Adam25T <turner.adam025@gmail.com>
2016-08-10 19:22:33 -0400

    Added job class and db migration

---
+++ STAR.Domain/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STAR.Domain {
+    public class Job {
+
+        public int JobId { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool isFilled { get; set; } 
+    }
+}
