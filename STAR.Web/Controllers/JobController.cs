e1b3ab0a5b4a675ca947c5666c3471ebf820c0b8
Adam25T <turner.adam025@gmail.com>
2016-08-11 13:19:44 -0400

    Added Job controller, model, and list view

---
+++ STAR.Web/Controllers/JobController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using STAR.Data;
+
+namespace STAR.Web.Controllers
+{
+    public class JobController : Controller {
+        private StarContext context;
+
+        public JobController(DbContext context) {
+            this.context = context as StarContext;
+        }
+        // GET: Job
+        public ActionResult Index()
+        {
+            return View(context.Jobs);
+        }
+    }
+}
