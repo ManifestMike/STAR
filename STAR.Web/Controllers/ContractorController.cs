2384c3043093b6834d43b430c8ff3ed9d9e1635e
Adam25T <turner.adam025@gmail.com>
2016-08-12 15:32:10 -0400

    forgot a } and pushed broken code

--- STAR.Web/Controllers/ContractorController.cs
+++ STAR.Web/Controllers/ContractorController.cs
@@ -140,7 +140,7 @@ namespace STAR.Web.Controllers {
             context.SaveChanges();
             return RedirectToAction("Index");
         }
-        
+
         public ActionResult AvailableContractors() {
             var positions = context.Positions;
             var availableContractors = context.Contractors
@@ -148,6 +148,7 @@ namespace STAR.Web.Controllers {
                     .Select(p => p.contractorId)
                     .Contains(c.ID));
             return View("Index", availableContractors);
+        }
 
         private ActionResult GetIndexView()
         {
