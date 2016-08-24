fdc2c44a94e73af85ce8cb12b719d7dcaa5f6a49
Joel S Sierra <jsierra@student.cscc.edu>
2016-08-24 10:19:09 -0400

    model complete

--- STAR.Web/Models/LoginModel.cs
+++ STAR.Web/Models/LoginModel.cs
@@ -1,9 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace STAR.Web.Models {
     public class LoginModel {
+        [Key]
+        public int UserID { get; set; }
+        
+        [Required(ErrorMessage = "Last name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords must match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
     }
 }
